294e6634c607b28190c9b3242db2ec5dc496af2e
Florian Ewald <wuwu@gmx.net>
2018-02-07 10:59:07 +0100

    updated seo description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Gesichtsbehandlungen & Wellness Massagen mit Naturkosmetik in Chur - Schweiz' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
