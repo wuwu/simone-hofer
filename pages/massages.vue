b3ca19988456f38220c2aa8cfa44bc80f2c56913
Florian Ewald <wuwu@gmx.net>
2018-02-12 17:58:18 +0100

    backend: name: git

---
+++ pages/massages.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wrapper">
+        <logo/>
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <p>{{ }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+
+export default {
+  components: {
+    Logo
+  },
+  asyncData: async ({ app }) => ({
+    post: await app.$content('/pages').get('/massages'),
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
